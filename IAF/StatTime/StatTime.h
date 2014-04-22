6bd6bb7c74c6b6f5522d09b0a11d02ef6c5d9c9b
izhuxin <izhuxin@gmail.com>
2014-04-22 18:30:07 +0800

    StatTime

---
+++ IAF/StatTime/StatTime.h
@@ -0,0 +1,38 @@
+#ifndef _STATTIME_H_0A57F90C_659F_4551_8455_3349085AB3D5
+#define _STATTIME_H_0A57F90C_659F_4551_8455_3349085AB3D5
+
+/*
+	author   : izhuxin
+	function : statistical time
+*/
+
+#include <windows.h>
+#include <assert.h>
+#include <time.h>
+
+class StatTime
+{
+public:
+	StatTime();
+	~StatTime();
+	void Start();
+	void Stop();
+	void ReportServiceTime();
+public:
+	static StatTime& GetInstance()
+	{
+		static StatTime instance;
+		return instance;
+	}
+	enum{SECONDSINMINUTE = 60};
+
+private:
+	clock_t  begin;
+	clock_t  end;
+	int elapsedMinutes;
+};
+
+#define StatTimeInstance StatTime::GetInstance()
+
+
+#endif
