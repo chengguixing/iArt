26f27674ac075673491b1e3cbed9f644477928c6
izhuxin <izhuxin@gmail.com>
2014-04-25 19:16:47 +0800

    GetDiskFreeSpaceEx

--- IAF/GetDiskFreeSpaceEx/main.cpp
+++ IAF/GetDiskFreeSpaceEx/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <windows.h>
 #include <stdlib.h>
 #include <iostream>
@@ -17,4 +18,52 @@ int main(void)
 
 	system("pause");
 	return 0;
+}
+*/
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	/*
+    BOOL bFlag;
+    TCHAR Buf[256];    
+
+    bFlag = GetVolumeNameForVolumeMountPoint(
+        _T("E:\\X\\"),
+        Buf,
+        256
+        );
+
+    if (bFlag != TRUE) 
+    {
+        return (-2);
+    }
+	else
+	{
+		printf("%s\n", Buf);
+		ULARGE_INTEGER nTotalNumberOfBytes;
+		ULARGE_INTEGER nTotalNumberOfFreeBytes;
+
+		GetDiskFreeSpaceEx(Buf, NULL, (PULARGE_INTEGER)&nTotalNumberOfBytes, (PULARGE_INTEGER)&nTotalNumberOfFreeBytes );
+
+		cout << (nTotalNumberOfBytes.QuadPart >> 30) << endl;
+		cout << (nTotalNumberOfFreeBytes.QuadPart >> 30) << endl;
+	}
+	*/
+	ULARGE_INTEGER nTotalNumberOfBytes;
+	ULARGE_INTEGER nTotalNumberOfFreeBytes;
+
+	GetDiskFreeSpaceEx(L"E:\\X\\", NULL, (PULARGE_INTEGER)&nTotalNumberOfBytes, (PULARGE_INTEGER)&nTotalNumberOfFreeBytes );
+	signed __int64 DiskFreeSpace = nTotalNumberOfFreeBytes.QuadPart;
+	cout << nTotalNumberOfFreeBytes.QuadPart << endl;
+	cout << nTotalNumberOfBytes.QuadPart << endl;
+	cout << DiskFreeSpace << endl;
+	
+	system("pause");
+    return 0;
 }
