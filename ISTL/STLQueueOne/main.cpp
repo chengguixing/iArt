532551fb4d483662f09f864a64d7a8178e352922
izhuxin <izhuxin@gmail.com>
2014-05-06 08:03:18 +0800

    deque

---
+++ ISTL/STLQueueOne/main.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+#include <list>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	queue<int, list<int> > iqueue;
+	iqueue.push(1);
+	iqueue.push(3);
+	iqueue.push(5);
+	iqueue.push(7);
+
+	cout << iqueue.size() << endl;
+	cout << iqueue.front() << endl;
+
+	iqueue.pop();
+	cout << iqueue.front() << endl;
+
+	iqueue.pop();
+	cout << iqueue.front() << endl;
+
+	iqueue.pop();
+	cout << iqueue.front() << endl;
+
+	cout << iqueue.size() << endl;
+
+	system("pause");
+	return 0;
+
+}
---
+++ ISTL/STLStackOne/main.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+#include <list>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	stack<int, list<int> > istack;
+	istack.push(1);
+	istack.push(3);
+	istack.push(5);
+	istack.push(7);
+
+
+	cout << istack.size() << endl;
+	cout << istack.top() << endl;
+
+	istack.pop();
+	cout << istack.top() << endl;
+	istack.pop();
+	cout << istack.top() << endl;
+	istack.pop();
+	cout << istack.top() << endl;
+
+	cout << istack.size() << endl;
+
+
+	system("pause");
+	return 0;
+}
